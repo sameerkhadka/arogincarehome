52a16594abe9f3cb42047090551a5bdb53035c3d
sameerkhadka <sameerkhadka3@gmail.com>
2021-09-12 11:01:46 +0545

    design improvement

--- resources/views/vendor/voyager/blogs/browse.blade.php
+++ resources/views/vendor/voyager/blogs/browse.blade.php
@@ -45,7 +45,6 @@
                         <label class="control-label" for="name">Meta Description</label>
                         <textarea class="form-control" name="meta_description" rows="5">{{$item->meta_description}}</textarea>                        
                     </div>
-                    <div class="row">
                         <div class="form-group col-md-9" >
                             <label class="control-label" for="name">Banner Text</label>
                             <input type="text" class="form-control" id="title" name="title" value="{{$item->title}}">
@@ -54,8 +53,7 @@
                             <label class="control-label" for="name">Banner Image</label>
                             <img src="{{asset('storage/pages/September2021/' . $item->image)}}" style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;">
                             <input type="file" name="image">
-                        </div>                    
-                    </div>
+                        </div>  
                     <input type="submit" value="save" class="btn btn-success btn-add-new">
                 </form>
             </div>
